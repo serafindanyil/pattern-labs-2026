27fb85b40fdd4ccb9f93680fea7454225888084e
Danyil Serafin <serafin.danyil14@gmail.com>
2026-04-26 19:48:40 +0300

    feat: init Next.js project with dependencies

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts", "*.cjs"]),
+  {
+    files: ["src/shared/api/generated-api.ts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+]);
+
+export default eslintConfig;
